a5f090277e7e8f3bb9728c9d01c4c0910f8ba1db
Haruite <89329576+Haruite@users.noreply.github.com>
2024-03-14 20:26:13 +0800

    Update qb.py

--- client/qb.py
+++ client/qb.py
@@ -11,7 +11,14 @@ class Qbittorrent(BTClient):
         self.client.torrents_rename_file(torrent_hash=torrent_hash, old_path=old_path, new_path=new_path)
 
     def rename_folder(self, torrent_hash, old_folder, new_folder):
-        self.client.torrents_rename_folder(torrent_hash, old_path=old_folder, new_path=new_folder)
+        try:
+            self.client.torrents_rename_folder(torrent_hash, old_path=old_folder, new_path=new_folder)
+        except:
+            for file in self.client.torrents_files(torrent_hash):
+                old_name = file.name
+                new_name = old_name.replace(old_folder, new_folder)
+                if old_name != new_name:
+                    self.client.torrents_rename_file(torrent_hash=torrent_hash, file_id=file.index, new_file_name=new_name)
 
     def get_hashes(self):
         return {torrent.hash for torrent in self.client.torrents_info()}
